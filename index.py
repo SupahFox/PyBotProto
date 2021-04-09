53312470f76fd89cab8fa072f1120f8728e259fb
Fabri <fabuz313@gmail.com>
2021-04-09 12:04:05 -0300

    a

--- index.py
+++ index.py
@@ -2,25 +2,32 @@
 import os
 
 import discord
+from discord.ext import commands
 from dotenv import load_dotenv
 
 load_dotenv()
 
 client = discord.Client()
 
+#PREFIX
+prefix= os.getenv('PREFIX')
+
+#COMMANDS
 @client.event
 async def on_message(message):
   if message.author == client.user:
     return
 
-  if message.author == client.user:
-    return
-
-  if message.content.startswith('py!hello'):
+  if message.content.startswith(prefix + 'hello'):
     await message.channel.send('Hello!')
 
+  if message.content.startswith('py!hello'):
+    await message.channel.send('Esto es una prueba de la libreria discord.py que posiblemente se agregue al bot principal. Ignoren esto.')
+
+#READY
 @client.event
 async def on_ready():
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="the game"))
     print(f'{client.user} Se conectó a discord!')
 
 client.run(os.getenv('TOKEN'))
